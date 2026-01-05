bec3547330e0ad3d2773d995119e6f107ba1ca22
EliaLaussegger <elia.laussegger@gmail.com>
2026-01-05 22:51:27 +0100

    Add execution time measurement to Program.cs
    Introduced Stopwatch to measure and display the elapsed time for the Id parsing operation in Main. Also cleaned up formatting and commented out the previous Part 1 implementation for reference.

--- 2025/Cal_Day5/Cal_Day5/Cal_Day5/Program.cs
+++ 2025/Cal_Day5/Cal_Day5/Cal_Day5/Program.cs
@@ -1,12 +1,61 @@
-﻿class Programm
+﻿//Part 1
+//class Programm
+//{
+//    static void Main(string[] args)
+//    {
+//        Id id = new Id();
+//        id.parse();
+//        Console.WriteLine(id.counter);
+//    }
+//}
+//class Id
+//{
+//    public int counter = 0;
+//    public void parse()
+//    {
+//        string input = System.IO.File.ReadAllText("Input.txt");
+//        string inputId = System.IO.File.ReadAllText("InputId.txt");
+//        string[] lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+//        string[] linesIdarray = inputId.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+//        List<string> linesIdList = new List<string>(linesIdarray);
+
+//        foreach (string line in lines)
+//        {
+//            string[] range = line.Split('-', StringSplitOptions.RemoveEmptyEntries);
+//            int j = linesIdList.Count;
+//            for (int i = 0 ; i < j; i++)
+//            {
+//                if (Convert.ToInt64(linesIdList[i]) >= Convert.ToInt64(range[0]) && Convert.ToInt64(linesIdList[i]) <= Convert.ToInt64(range[1]))
+//                {
+//                    counter++;
+//                    linesIdList.RemoveAt(i);
+//                    i--;
+//                    j = linesIdList.Count;
+//                }
+//            }
+
+
+
+//        }
+//    }
+//}
+using System.Diagnostics;
+
+class Programm
 {
     static void Main(string[] args)
     {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
         Id id = new Id();
         id.parse();
+        sw.Stop();
         Console.WriteLine(id.counter);
+        TimeSpan vergangeneZeit = sw.Elapsed;
+        Console.WriteLine($"Vergangen: {vergangeneZeit.TotalMilliseconds} ms");
     }
 }
+
 class Id
 {
     public int counter = 0;
@@ -22,7 +71,7 @@ class Id
         {
             string[] range = line.Split('-', StringSplitOptions.RemoveEmptyEntries);
             int j = linesIdList.Count;
-            for (int i = 0 ; i < j; i++)
+            for (int i = 0; i < j; i++)
             {
                 if (Convert.ToInt64(linesIdList[i]) >= Convert.ToInt64(range[0]) && Convert.ToInt64(linesIdList[i]) <= Convert.ToInt64(range[1]))
                 {
@@ -32,7 +81,7 @@ class Id
                     j = linesIdList.Count;
                 }
             }
-   
+
 
         }
     }
